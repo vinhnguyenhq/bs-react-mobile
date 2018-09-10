e703068685ec227d6c788de2d41831c16462b2f1
Hoang Quoc Vinh Nguyen <quocvinh.nguyen@grabtaxi.com>
2018-09-10 14:16:57 +0700

    add bs modal to check on safari phone

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -13,6 +13,200 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+
+        <div
+          className="modal fade show"
+          tabIndex="-1"
+          role="dialog"
+          style={{ display: "block" }}
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Modal title</h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div
+                className="modal-body"
+                style={{ overflow: "auto", height: "400px" }}
+              >
+                <div className="alert alert-success" role="alert">
+                  <h4 className="alert-heading">Well done!</h4>
+                  <p>
+                    Aww yeah, you successfully read this important alert
+                    message. This example text is going to run a bit longer so
+                    that you can see how spacing within an alert works with this
+                    kind of content.
+                  </p>
+                  <hr />
+                  <p className="mb-0">
+                    Whenever you need to, be sure to use margin utilities to
+                    keep things nice and tidy.
+                  </p>
+                </div>
+                <div className="alert alert-success" role="alert">
+                  <h4 className="alert-heading">Well done!</h4>
+                  <p>
+                    Aww yeah, you successfully read this important alert
+                    message. This example text is going to run a bit longer so
+                    that you can see how spacing within an alert works with this
+                    kind of content.
+                  </p>
+                  <hr />
+                  <p className="mb-0">
+                    Whenever you need to, be sure to use margin utilities to
+                    keep things nice and tidy.
+                  </p>
+                </div>
+                <div className="alert alert-success" role="alert">
+                  <h4 className="alert-heading">Well done!</h4>
+                  <p>
+                    Aww yeah, you successfully read this important alert
+                    message. This example text is going to run a bit longer so
+                    that you can see how spacing within an alert works with this
+                    kind of content.
+                  </p>
+                  <hr />
+                  <p className="mb-0">
+                    Whenever you need to, be sure to use margin utilities to
+                    keep things nice and tidy.
+                  </p>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
